74c4d3bf845261bb02bf22d36d0f8260c2f1f60f
frusciante189 <53580986+frusciante189@users.noreply.github.com>
2021-11-10 17:02:51 +0300

    date updated

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
   let interval = useRef();
 
   const startTimer = () => {
-    const countdownDate = new Date("Oct 16, 2021 04:00:00").getTime();
+    const countdownDate = new Date("Nov 16, 2021 04:00:00").getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
